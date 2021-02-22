9fbd7ca67eed1ed64968d9fd8c92a393bbf41b35
Eddie-pd <eddiepd.w@gmail.com>
2021-02-22 13:49:47 +0100

    Not Exponential - model

--- NOT EXPONENTIAL.py
+++ NOT EXPONENTIAL.py
@@ -24,9 +24,10 @@ from astropy.coordinates import solar_system_ephemeris
 from poliastro.twobody.propagation import propagate, cowell
 from poliastro.ephem import build_ephem_interpolant
 from poliastro.core.elements import rv2coe
+from poliastro.atmosphere import COESA76
 
 from poliastro.constants import rho0_earth, H0_earth
-from poliastro.core.perturbations import atmospheric_drag_exponential,  atmospheric_drag_model, third_body, J2_perturbation
+from poliastro.core.perturbations import atmospheric_drag_exponential, atmospheric_drag_model, third_body, J2_perturbation
 from poliastro.bodies import Earth, Moon
 from poliastro.twobody import Orbit
 from poliastro.plotting import OrbitPlotter3D
@@ -42,8 +43,8 @@ k = Earth.k.to(u.km ** 3 / u.s ** 2).value
 C_D = 2.2
 A_over_m = (((4 * np.pi * 0.0144) / 4.0) * (u.m ** 2) / (48 * u.kg)).to_value(u.km ** 2 / u.kg)
 B = C_D * A_over_m
-rho0 = rho0_earth.to(u.kg / u.km ** 3).value
-H0 = H0_earth.to(u.km).value
+#rho0 = rho0_earth.to(u.kg / u.km ** 3).value
+#H0 = H0_earth.to(u.km).value
 
 
 
@@ -54,22 +55,24 @@ from poliastro.twobody.events import LithobrakeEvent
 lithobrake_event = LithobrakeEvent(R)
 events = [lithobrake_event]
 
-rr = propagate(
-        orbit,
+coesa76 = COESA76()
+
+rr, _ = cowell(
+        Earth.k,
+        orbit.r,
+        orbit.v,
         tofs,
-        method=cowell,
-        ad=atmospheric_drag_exponential,
+        ad=atmospheric_drag_model,
         R=R,
         C_D=C_D,
         A_over_m=A_over_m,
-        H0=H0,
-        rho0=rho0,
+        model = coesa76,
         events=events,
     )
 
 print('{0} {1:.2f} {2}'.format('orbital decay seen after', lithobrake_event.last_t.to(u.d).value, 'days'))
 
 
-plt.ylabel("h(t)")
-plt.xlabel("t, days")
-plt.plot(tofs.value, rr.norm() - Earth.R)
+#plt.ylabel("h(t)")
+#plt.xlabel("t, days")
+#plt.plot(tofs.value, rr, _.norm() - Earth.R)
